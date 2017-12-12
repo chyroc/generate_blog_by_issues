2c1ecbcfe2451700d79595944a28c563da3af6a3
Chyroc <chen_yunpeng@foxmail.com>
2017-12-13 01:36:22 +0800

    add version (#4)

--- README.md
+++ README.md
@@ -9,10 +9,5 @@ go get -u github.com/chyroc/generate_blog_by_issues
 
 # use
 ```
-generate_blog_by_issues -repo chyroc/chyroc.github.io
-```
-
-# use with token
-```
-generate_blog_by_issues -repo chyroc/chyroc.github.io -t <github_personal_token>
+generate_blog_by_issues -repo chyroc/chyroc.github.io -token <github_personal_token>
 ```
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Chyroc/generate_blog_by_issues/internal"
 )
@@ -10,7 +12,13 @@ import (
 func getCommandLine() (string, string) {
 	repo := flag.String("repo", "", "where the repo is")
 	token := flag.String("token", "", "github token")
+	v := flag.Bool("v", false, "generate_blog_by_issues version")
 	flag.Parse()
+
+	if *v {
+		fmt.Printf("generate_blog_by_issues 0.1.0\n")
+		os.Exit(0)
+	}
 
 	if *repo == "" {
 		log.Fatal("must set github repo where issue is")
